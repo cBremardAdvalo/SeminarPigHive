2138a4691a20032722cdc1312f70d1b372332cd4
cbremard <cbremard@advalo.com>
2016-11-20 13:41:31 +0100

    move files

---
+++ Sources/dataGenerator/src/main/java/ihm/Activity01.java
@@ -0,0 +1,80 @@
+package ihm;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Activity01 extends Activity<String> {
+	private static final long serialVersionUID = 1L;
+	private static int xSize;
+	private static int ySize;
+
+	private Image requestImage() {
+        Image image = null;
+        try {
+            image = ImageIO.read(getClass().getResource("/resources/groupchatui.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return image;
+    }
+
+	@Override
+	public String run(Map<String, String> globalMap) {
+		final Image image = requestImage();
+		final JFrame frame = new JFrame("My First Chat !");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel() {
+			private static final long serialVersionUID = 1L;
+			@Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, null);
+            }
+        };
+        xSize = image.getWidth(frame);
+        ySize = image.getHeight(frame) + 45;
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JButton button = new JButton("Let's GO !!!");
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setFont(new Font("Calibry", Font.PLAIN, 20));
+        button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+			}
+		});
+        panel.add(Box.createRigidArea(new Dimension(15, ySize)));
+        panel.add(button);
+
+        panel.setPreferredSize(new Dimension(xSize,ySize+45));
+
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        while(frame.isVisible()){
+        	sleep(10);
+        	//frame.setVisible(false);
+        }
+		return getClass().getName();
+	}
+
+
+}
---
+++ Sources/dataGenerator/src/main/java/ihm/Activity05.java
@@ -0,0 +1,66 @@
+package ihm;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.ensai.dataGenerator.App;
+
+public class Activity05 extends Activity<String> {
+	private static final long serialVersionUID = 1L;
+	private final JPanel jpanel;
+	private final JFileChooser fc;
+	
+	  // Create a form with the specified labels, tooltips, and sizes.
+	public Activity05() {
+		jpanel = new JPanel(new BorderLayout());
+        JLabel lab = new JLabel("Veuillez choisir un dossier pour stocker les données", JLabel.CENTER);
+        jpanel.add(lab , BorderLayout.CENTER);
+		fc = new JFileChooser();
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		fc.setMultiSelectionEnabled(false);
+		fc.setControlButtonsAreShown(false);
+		jpanel.add(fc);
+	}
+
+	 
+	@Override
+	public String run(Map<String, String> globalMap) {
+	    final JFrame frame = new JFrame("Setting 4/4");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.getContentPane().add(jpanel, BorderLayout.NORTH);
+	    JPanel panel = new JPanel();
+	    JButton submit = new JButton("Finish");
+	    submit.addActionListener(new ActionListener() {
+	      public void actionPerformed(ActionEvent e) {
+	    	  frame.setVisible(fc.getSelectedFile()==null);
+	      }
+	    });
+	    panel.add(submit);
+		System.out.println(Thread.currentThread().getStackTrace()[1]);
+	    frame.getContentPane().add(panel, BorderLayout.SOUTH);
+		System.out.println(Thread.currentThread().getStackTrace()[1]);
+	    frame.pack();
+		System.out.println(Thread.currentThread().getStackTrace()[1]);
+        frame.setLocationRelativeTo(null);
+		System.out.println(Thread.currentThread().getStackTrace()[1]);
+	    frame.setVisible(true);
+		System.out.println(Thread.currentThread().getStackTrace()[1]);
+        while(frame.isVisible()){
+        	sleep(10);
+        }
+		System.out.println(Thread.currentThread().getStackTrace()[0]);
+        globalMap.put(App.KEY_STAGING_PATH,fc.getSelectedFile().getPath());
+		System.out.println(Thread.currentThread().getStackTrace()[0]);
+		return this.getClass().getName();
+	}
+
+	}
+
